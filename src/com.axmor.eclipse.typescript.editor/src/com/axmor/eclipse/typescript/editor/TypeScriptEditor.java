3e39556ef52e788efb9a6314444ab853a6be9fcc
Asya Vorobyova <Asya Vorobyova@localhost>
2014-02-20 15:10:50 +0700

    Open Type functionality added

--- src/com.axmor.eclipse.typescript.editor/src/com/axmor/eclipse/typescript/editor/TypeScriptEditor.java
+++ src/com.axmor.eclipse.typescript.editor/src/com/axmor/eclipse/typescript/editor/TypeScriptEditor.java
@@ -136,7 +136,7 @@ public class TypeScriptEditor extends TextEditor implements IDocumentProcessor {
         doc.addDocumentListener(listener);
         ((TypeScriptEditorConfiguration) getSourceViewerConfiguration()).setFile(file);
         ((TypeScriptEditorConfiguration) getSourceViewerConfiguration()).setEditor(this);
-        String[] results = com.axmor.eclipse.typescript.core.Activator.getDefault().getSearchResults("Student");
+        //TypeDocument[] results = com.axmor.eclipse.typescript.core.Activator.getDefault().getSearchResults("Student");
     }
 
     /**
