460cda5829337b333fb60da2b995d88020a9911a
Konstantin Zaitcev <kos@axmor.com>
2014-03-13 09:24:49 +0700

    fix bridge debug logging

--- src/com.axmor.eclipse.typescript.core/lib/bridge/log.js
+++ src/com.axmor.eclipse.typescript.core/lib/bridge/log.js
@@ -1,6 +1,5 @@
 var logLevel = require('./args.js').logLevel;
 var level = logLevel === 'error' ? 2 : (logLevel === 'info' ? 1 : 0);
-level = 0;
 
 exports.error = console.error;
 exports.info  = level > 0 ? console.log : empty;
