2662188281859e3a29f1a2060ae6d18b3d18f381
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2014-09-09 12:14:06 +0900

    Use UTF-8 in bridge I/O.

--- src/com.axmor.eclipse.typescript.core/src/com/axmor/eclipse/typescript/core/internal/TypeScriptBridge.java
+++ src/com.axmor.eclipse.typescript.core/src/com/axmor/eclipse/typescript/core/internal/TypeScriptBridge.java
@@ -12,7 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.CountDownLatch;
@@ -255,12 +255,12 @@ public class TypeScriptBridge implements Runnable {
                     obj.put("params", jsonParams);
                 }
 
-                try (PrintWriter writer = new PrintWriter(socket.getOutputStream())) {
-                    writer.write(obj.toString());
+                try (PrintStream writer = new PrintStream(socket.getOutputStream(), false, "UTF-8")) {
+                    writer.print(obj.toString());
                     writer.flush();
                     socket.shutdownOutput();
 
-                    try (InputStreamReader reader = new InputStreamReader(socket.getInputStream())) {
+                    try (InputStreamReader reader = new InputStreamReader(socket.getInputStream(), "UTF-8")) {
                         String str = CharStreams.toString(reader);
                         if ("null".equals(str)) {
                             return EMPTY_JSON_OBJECT;
