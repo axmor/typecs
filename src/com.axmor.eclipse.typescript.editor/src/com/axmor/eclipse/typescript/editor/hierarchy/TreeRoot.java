54367a783f1757ca5898ca05035534dcd59bb610
kudrin_p <kudrin_p@localhost>
2014-12-19 17:26:26 +0600

    ETSP-88: Implement "Call Hierarchy" view (#28)

---
+++ src/com.axmor.eclipse.typescript.editor/src/com/axmor/eclipse/typescript/editor/hierarchy/TreeRoot.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Axmor Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/ 
+package com.axmor.eclipse.typescript.editor.hierarchy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.Position;
+import org.eclipse.swt.graphics.Image;
+
+import us.monoid.json.JSONArray;
+import us.monoid.json.JSONException;
+import us.monoid.json.JSONObject;
+
+import com.axmor.eclipse.typescript.core.Activator;
+import com.axmor.eclipse.typescript.core.TypeScriptAPI;
+import com.axmor.eclipse.typescript.core.TypeScriptUtils;
+import com.axmor.eclipse.typescript.editor.TypeScriptEditorUtils;
+import com.axmor.eclipse.typescript.editor.TypeScriptUIImages;
+import com.google.common.base.Throwables;
+
+/**
+ * @author kudrin
+ *
+ */
+public class TreeRoot {
+    
+    private TreeRoot[] childItems = null;
+    private String name = null;    
+    private Image image;
+    private int offset;
+    private TypeScriptAPI api;
+    private IFile file;
+    private IFile[] projectFiles;
+    private int callOffset;
+    private int callLength;
+    
+    public TreeRoot(TypeScriptAPI api, JSONObject obj, int callOffset, int callLength, IFile currentFile, IFile[] projectFiles) {
+        this.name = getText(obj);
+        this.image = createImage(obj);
+        this.file = currentFile;
+        this.projectFiles = projectFiles;
+        this.api = api;
+        this.callOffset = callOffset;
+        this.callLength = callLength;
+        try {
+            Position position = TypeScriptEditorUtils.getPosition(obj);
+            this.offset = position.offset;
+            this.childItems = createChildren();
+        } catch (JSONException e) {
+            Activator.error(e);
+        }
+    }
+    
+    public TreeRoot[] getChildren() {
+        return childItems;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Image getImage() {        
+        return image;
+    }
+    
+    public int getOffset() {
+        return this.offset;
+    }
+    
+    public int getCallOffset() {
+        return this.callOffset;
+    }
+    
+    public int getCallLength() {
+        return this.callLength;
+    }
+    
+    public IFile getFile() {
+        return this.file;
+    }
+    
+    public boolean hasChildren() {
+        return childItems != null && childItems.length > 0;
+    }
+    
+    private String getText(JSONObject obj) {
+        try {
+            if (TypeScriptUtils.isTypeScriptLegacyVersion()) {
+                return obj.getString("name");
+            } else {
+                return obj.getString("text");
+            }
+        } catch (JSONException e) {
+            Activator.error(e);
+            return "";
+        }        
+    }
+    
+    private TreeRoot[] createChildren() {
+        JSONArray references = api.getReferencesAtPosition(file, offset);
+        IFile currentFile = null;
+        List<TreeRoot> roots = new ArrayList<TreeRoot>();
+        for (int i = 0; i < references.length(); i++) {
+            try {
+                if (references.get(i) instanceof JSONObject) {
+                    JSONObject obj = (JSONObject) references.get(i);
+                    String fileName = obj.getString("fileName");
+                    int segmentsCount = fileName.split("/").length;
+                    currentFile = null;
+                    for (int j = 0; j < projectFiles.length; j++) {
+                        if (projectFiles[j].getFullPath().segmentCount() < segmentsCount)
+                            continue;
+                        if (projectFiles[j].getFullPath()
+                                .removeFirstSegments(projectFiles[j].getFullPath().segmentCount() - segmentsCount)
+                                .toString().equals(fileName)) {
+                            currentFile = projectFiles[j];
+                            break;
+                        }
+                    }
+                    if (currentFile == null) {
+                        continue;
+                    }
+                    Position position = TypeScriptEditorUtils.getPosition(obj);
+                    JSONArray model = api.getScriptModel(currentFile);                    
+                    for (int j = 0; j < model.length(); j++) {
+                        if (model.get(j) instanceof JSONObject) {
+                            JSONObject item = (JSONObject) model.get(j);
+                            roots.addAll(fetchChildren(item.getJSONArray("childItems"), position.offset, position.length, currentFile));                         
+                        }
+                    }
+                }
+            } catch (JSONException e) {                
+                throw Throwables.propagate(e);
+            }
+        }         
+        return roots.toArray(new TreeRoot[roots.size()]);
+    }
+    
+    private List<TreeRoot> fetchChildren(JSONArray childs, int offset, int length, IFile currentFile) {
+        List<TreeRoot> newRoots = new ArrayList<TreeRoot>();
+        for (int i = 0; i < childs.length(); i++) {
+            try {
+                JSONObject obj = (JSONObject) childs.get(i);
+                Position itemPosition = TypeScriptEditorUtils.getPosition(obj);
+                if (offset > itemPosition.offset && offset < itemPosition.offset + itemPosition.length ) {
+                    if (obj.getJSONArray("childItems").length() > 0) {
+                        fetchChildren(obj.getJSONArray("childItems"), offset, length, currentFile);
+                    }
+                    else {
+                        newRoots.add(new TreeRoot(api, obj, offset, length, currentFile, projectFiles));
+                    }                    
+                }
+            } catch (JSONException e) {
+                Activator.error(e);
+            }
+        }
+        return newRoots;
+    }
+    
+    private Image createImage(JSONObject element) {
+        JSONObject obj = (JSONObject) element;
+        TypeScriptUIImages imagesFactory = new TypeScriptUIImages();
+        return imagesFactory.getImageForModelObject(obj);
+    }     
+
+}
---
+++ src/com.axmor.eclipse.typescript.editor/src/com/axmor/eclipse/typescript/editor/hierarchy/TypeScriptHierarchyUI.java
@@ -0,0 +1,181 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Axmor Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/ 
+package com.axmor.eclipse.typescript.editor.hierarchy;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.Position;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import us.monoid.json.JSONArray;
+import us.monoid.json.JSONException;
+import us.monoid.json.JSONObject;
+
+import com.axmor.eclipse.typescript.core.Activator;
+import com.axmor.eclipse.typescript.core.TypeScriptAPI;
+import com.axmor.eclipse.typescript.editor.TypeScriptEditor;
+import com.axmor.eclipse.typescript.editor.TypeScriptEditorUtils;
+
+/**
+ * @author kudrin
+ *
+ */
+public class TypeScriptHierarchyUI {
+    
+    private static TypeScriptHierarchyUI fgInstance;
+
+    private int fViewCount = 0;    
+    
+    private List<CallHierarchyViewPart> fCallHierarchyViews = new ArrayList<CallHierarchyViewPart>();
+    
+    private TypeScriptHierarchyUI() {
+        // Do nothing
+    }
+
+    public static TypeScriptHierarchyUI getDefault() {
+        if (fgInstance == null) {
+            fgInstance = new TypeScriptHierarchyUI();
+        }
+
+        return fgInstance;
+    }
+    
+    public static CallHierarchyViewPart openView(TypeScriptEditor editor, IFile[] projectFiles, IFile file, 
+            JSONArray references) {
+        IWorkbenchWindow window = editor.getSite().getWorkbenchWindow();
+        TypeScriptAPI api = editor.getApi();
+        IWorkbenchPage page= window.getActivePage();
+        try {
+            CallHierarchyViewPart viewPart= getDefault().findCallHierarchyViewPart(page);
+            String secondaryId= null;
+            if (viewPart == null) {
+                if (page.findViewReference(CallHierarchyViewPart.ID_CALL_HIERARCHY) != null) {
+                    secondaryId = String.valueOf(++getDefault().fViewCount);
+                }                
+            } else {
+                secondaryId = viewPart.getViewSite().getSecondaryId();
+            }                
+            viewPart = (CallHierarchyViewPart)page.showView(CallHierarchyViewPart.ID_CALL_HIERARCHY, secondaryId, 
+                    IWorkbenchPage.VIEW_ACTIVATE);
+            viewPart.setInputElements(createRoot(api, projectFiles, file, references));
+            return viewPart;
+        } catch (CoreException e) {
+            Activator.error(e);
+        }        
+        return null;
+    }
+    
+    private CallHierarchyViewPart findCallHierarchyViewPart(IWorkbenchPage page) {
+        for (Iterator<CallHierarchyViewPart> iter= fCallHierarchyViews.iterator(); iter.hasNext();) {
+            CallHierarchyViewPart view= iter.next();
+            if (page.equals(view.getSite().getPage())) {
+                return view;
+            }
+        }
+        IViewReference[] viewReferences= page.getViewReferences();
+        for (int i= 0; i < viewReferences.length; i++) {
+            IViewReference curr= viewReferences[i];
+            if (CallHierarchyViewPart.ID_CALL_HIERARCHY.equals(curr.getId()) && page.equals(curr.getPage())) {
+                CallHierarchyViewPart view= (CallHierarchyViewPart)curr.getView(true);
+                if (view != null) {
+                    return view;
+                }
+            }
+        }
+        return null;
+    }
+    
+    private static  TreeRoot[] createRoot(TypeScriptAPI api, IFile[] projectFiles, IFile file, JSONArray references) {
+        IFile currentFile = null;
+        for (int i = 0; i < references.length(); i++) {
+            try {
+                if (references.get(i) instanceof JSONObject) {
+                    JSONObject obj = (JSONObject) references.get(i);
+                    String fileName = obj.getString("fileName");
+                    int segmentsCount = fileName.split("/").length;
+                    currentFile = null;
+                    for (int j = 0; j < projectFiles.length; j++) {
+                        if (projectFiles[j].getFullPath().segmentCount() < segmentsCount)
+                            continue;
+                        if (projectFiles[j].getFullPath()
+                                .removeFirstSegments(projectFiles[j].getFullPath().segmentCount() - segmentsCount)
+                                .toString().equals(fileName)) {
+                            currentFile = projectFiles[j];
+                            break;
+                        }
+                    }
+                    if (currentFile == null) {
+                        continue;
+                    }
+                    Position position = TypeScriptEditorUtils.getPosition(obj);
+                    JSONArray model = api.getScriptModel(currentFile);                    
+                    for (int j = 0; j < model.length(); j++) {
+                        if (model.get(j) instanceof JSONObject) {
+                            JSONObject item = (JSONObject) model.get(j);
+                            JSONObject root = findInitCall(item, position.offset);
+                            if (root != null) {
+                                Position callPos = TypeScriptEditorUtils.getPosition(obj);
+                                return new TreeRoot[] {new TreeRoot(api, root, callPos.offset, callPos.length, currentFile, 
+                                        projectFiles)};
+                            }
+                        }
+                    }
+                }
+            } catch (JSONException e) {                
+                Activator.error(e);
+            }
+        }
+        return new TreeRoot[0];
+    }
+    
+    private static JSONObject findInitCall(JSONObject item, int offset) {
+        Position itemPosition;
+        JSONObject result = null;
+        try {
+            itemPosition = TypeScriptEditorUtils.getPosition(item);
+            if (itemPosition.offset == offset) {
+                result = item;
+            }
+            else {
+                JSONArray children = item.getJSONArray("childItems");
+                for (int i = 0; i < children.length(); i++) {
+                    JSONObject child = (JSONObject) children.get(i);
+                    result = findInitCall(child, offset);
+                    if (result != null) {
+                        break;
+                    }
+                }
+            }                        
+        } catch (JSONException e) {
+            Activator.error(e);
+        }
+        return result;
+    }
+
+    /**
+     * @param callHierarchyViewPart
+     */
+    public void callHierarchyViewActivated(CallHierarchyViewPart view) {
+        fCallHierarchyViews.remove(view);
+        fCallHierarchyViews.add(0, view);        
+    }
+
+    /**
+     * @param callHierarchyViewPart
+     */
+    public void callHierarchyViewClosed(CallHierarchyViewPart view) {
+        fCallHierarchyViews.remove(view);        
+    }
+
+}
